2865c629defb6f296c9f51dfddfebc7f242803be
ppippi <wjdqlsdlsp@naver.com>
2025-01-08 00:09:37 +0900

    test

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,15 @@
 import { getPostDatabase } from '@/notionApi/getPostDatabase'
 import { MetadataRoute } from 'next'
+import { PageObjectResponse, DatePropertyItemObjectResponse } from '@notionhq/client/build/src/api-endpoints'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://ppippi-dev.github.io'
   
-  // 노션 포스트 데이터 가져오기
   const posts = await getPostDatabase()
   
-  // 포스트 URL 생성
   const postsUrls = posts.results.map((post) => ({
     url: `${baseUrl}/post/${post.id}`,
-    lastModified: new Date(post.last_edited_time),
+    lastModified: new Date(((post as PageObjectResponse).properties.post_date as DatePropertyItemObjectResponse).date?.start ?? ''),
     changeFrequency: 'weekly' as const,
     priority: 0.7,
   }))
