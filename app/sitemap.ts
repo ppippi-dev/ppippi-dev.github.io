88a7953614c0d4262bb5f80ad8c4477f5be46edd
ppippi <wjdqlsdlsp@naver.com>
2025-01-29 22:02:33 +0900

    sitemap change

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,27 +1,27 @@
-import { getPostDatabase } from '@/notionApi/getPostDatabase'
-import { MetadataRoute } from 'next'
-import { PageObjectResponse, DatePropertyItemObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+// import { getPostDatabase } from '@/notionApi/getPostDatabase'
+// import { MetadataRoute } from 'next'
+// import { PageObjectResponse, DatePropertyItemObjectResponse } from '@notionhq/client/build/src/api-endpoints'
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://ppippi-dev.github.io'
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   const baseUrl = 'https://ppippi-dev.github.io'
   
-  const posts = await getPostDatabase()
+//   const posts = await getPostDatabase()
   
-  const postsUrls = posts.results.map((post) => {
-    const postDate = ((post as PageObjectResponse).properties.post_date as DatePropertyItemObjectResponse).date?.start
-    return {
-      url: `${baseUrl}/post/${post.id}`,
-      lastModified: postDate ? new Date(postDate) : new Date(),
-      priority: 0.8
-    }
-  })
+//   const postsUrls = posts.results.map((post) => {
+//     const postDate = ((post as PageObjectResponse).properties.post_date as DatePropertyItemObjectResponse).date?.start
+//     return {
+//       url: `${baseUrl}/post/${post.id}`,
+//       lastModified: postDate ? new Date(postDate) : new Date(),
+//       priority: 0.8
+//     }
+//   })
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      priority: 1.0
-    },
-    ...postsUrls,
-  ]
-} 
+//   return [
+//     {
+//       url: baseUrl,
+//       lastModified: new Date(),
+//       priority: 1.0
+//     },
+//     ...postsUrls,
+//   ]
+// } 
