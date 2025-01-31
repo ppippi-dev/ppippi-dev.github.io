0e5b7292953bed9b0aa3db7f2f1f12f4cecc1a57
ppippi <wjdqlsdlsp@naver.com>
2025-01-31 15:39:32 +0900

    time change

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -10,15 +10,15 @@ export async function GET() {
   try {
     const baseUrl = 'https://ppippi-dev.github.io'
     
-    // XML 헤더와 시작 태그
-    let xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
-    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
-    
-    // 메인 페이지 추가
-    xml += '<url>\n'
-    xml += `  <loc>${baseUrl}</loc>\n`
-    xml += `  <lastmod>${formatDate(new Date(new Date().getTime() - (9 * 60 * 60 * 1000)))}</lastmod>\n`
-    xml += '</url>\n'
+    // XML 문자열 생성 시작
+    const xmlStrings = [
+      '<?xml version="1.0" encoding="UTF-8"?>',
+      '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+      '<url>',
+      `  <loc>${baseUrl}</loc>`,
+      `  <lastmod>${formatDate(new Date(new Date().getTime() - (9 * 60 * 60 * 1000)))}</lastmod>`,
+      '</url>'
+    ];
 
     // 블로그 포스트 추가
     const posts = await getPostDatabase()
@@ -29,14 +29,18 @@ export async function GET() {
         ? formatDate(new Date(new Date(postDate).getTime() - (9 * 60 * 60 * 1000)))
         : formatDate(new Date(new Date().getTime() - (9 * 60 * 60 * 1000)))
 
-      xml += '<url>\n'
-      xml += `  <loc>${baseUrl}/post/${post.id}</loc>\n`
-      xml += `  <lastmod>${lastmod}</lastmod>\n`
-      xml += '</url>\n'
+      xmlStrings.push(
+        '<url>',
+        `  <loc>${baseUrl}/post/${post.id}</loc>`,
+        `  <lastmod>${lastmod}</lastmod>`,
+        '</url>'
+      );
     }
 
-    // XML 종료 태그
-    xml += '</urlset>'
+    xmlStrings.push('</urlset>');
+
+    // 배열을 줄바꿈으로 결합
+    const xml = xmlStrings.join('\n');
 
     // Response 헤더 설정과 함께 XML 반환
     return new Response(xml, {
@@ -50,13 +54,15 @@ export async function GET() {
     console.error('Sitemap generation failed:', error)
     
     // 에러 발생 시 기본 sitemap 반환
-    const fallbackXml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url>
-  <loc>https://ppippi-dev.github.io</loc>
-  <lastmod>${formatDate(new Date(new Date().getTime() - (9 * 60 * 60 * 1000)))}</lastmod>
-</url>
-</urlset>`
+    const fallbackXml = [
+      '<?xml version="1.0" encoding="UTF-8"?>',
+      '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+      '<url>',
+      `  <loc>https://ppippi-dev.github.io</loc>`,
+      `  <lastmod>${formatDate(new Date(new Date().getTime() - (9 * 60 * 60 * 1000)))}</lastmod>`,
+      '</url>',
+      '</urlset>'
+    ].join('\n');
 
     return new Response(fallbackXml, {
       headers: {
